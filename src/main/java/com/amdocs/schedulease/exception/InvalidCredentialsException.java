5a795bf40df493a65d9fa2d2a1b60d8c3d51caa8
dhirvivekdev <vdhir2002@gmail.com>
2025-10-08 11:58:15 +0530

    Made exceptions for module 1

---
+++ src/main/java/com/amdocs/schedulease/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.amdocs.schedulease.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
